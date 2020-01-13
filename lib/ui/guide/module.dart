0362130d538f2b7702bbd70de9274efe8aa5d45c
Andrey Molochko <Molochko_AG_16@student.grsu.by>
2020-01-13 17:25:12 +0300

    create mvvm arhitecture, add folders and base classes for guide activity

---
+++ lib/ui/guide/module.dart
@@ -0,0 +1,3 @@
+export 'package:sqflite_worker/ui/guide/view_model/guide_view_model_type.dart';
+export 'package:sqflite_worker/ui/guide/view_model/guide_view_model.dart';
+export 'package:sqflite_worker/ui/guide/widgets/guide_page.dart';
---
+++ lib/ui/guide/view_model/guide_view_model.dart
@@ -0,0 +1,3 @@
+import '../module.dart';
+
+class GuideViewModel implements GuideViewModelType {}
