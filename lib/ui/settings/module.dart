cf389c13fbfc246f71b088e704455db78d14ef9a
Andrey Molochko <Molochko_AG_16@student.grsu.by>
2020-01-27 13:01:19 +0300

    create structure of settings module

---
+++ lib/ui/settings/module.dart
@@ -0,0 +1,3 @@
+export 'package:sqflite_worker/ui/settings/view_model/settings_view_model.dart';
+export 'package:sqflite_worker/ui/settings/view_model/settings_view_model_type.dart';
+export 'package:sqflite_worker/ui/settings/widgets/settings_page.dart';
---
+++ lib/ui/settings/view_model/settings_view_model.dart
@@ -0,0 +1,5 @@
+import '../module.dart';
+
+class SettingsViewModel implements SettingsViewModelType {
+  
+}
