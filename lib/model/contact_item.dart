55a5d8b74754f4df6de531c8211cea4d28e2108a
Andrey Molochko <Molochko_AG_16@student.grsu.by>
2020-02-04 18:03:06 +0300

    add contact type

--- lib/model/contact_item.dart
+++ lib/model/contact_item.dart
@@ -1,11 +1,11 @@
 import 'package:sqflite_worker/model/module.dart';
 
-class Contact implements SettingsItem {
+class ContactItem implements SettingsItem {
   @override
   List subItems;
 
   @override
   String title;
 
-  Contact(this.subItems, this.title);
+  ContactItem(this.title, this.subItems);
 }
---
+++ lib/model/contact_type.dart
@@ -0,0 +1,3 @@
+enum ContactType {
+  linkedin, instagram, skype
+}
