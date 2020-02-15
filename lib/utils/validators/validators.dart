b3d4e0d2f53407902ef2b6294ecb9479dbcac405
Andrey Molochko <Molochko_AG_16@student.grsu.by>
2020-02-15 11:39:57 +0300

    create structure for validators module

---
+++ lib/utils/validators/validators.dart
@@ -0,0 +1,4 @@
+export 'package:sqflite_worker/utils/validators/email_validator.dart';
+export 'package:sqflite_worker/utils/validators/email_validator_type.dart';
+export 'package:sqflite_worker/utils/validators/password_validator.dart';
+export 'package:sqflite_worker/utils/validators/password_validator_type.dart';
