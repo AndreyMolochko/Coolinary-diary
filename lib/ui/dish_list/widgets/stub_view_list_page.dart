168ec8791ad0d16f0683d2145b0a04ef229abf41
Andrey Molochko <andrey.molochko2018@gmail.com>
2021-04-21 17:47:18 +0300

    Create localization for stubview screen

--- lib/ui/dish_list/widgets/stub_view_list_page.dart
+++ lib/ui/dish_list/widgets/stub_view_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_svg/svg.dart';
+import 'package:sqflite_worker/localization/app_translations.dart';
 import 'package:sqflite_worker/resourses/module.dart' as App;
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class StubViewListPage extends StatelessWidget {
               height: App.Dimens.sizeImageStubView, width: App.Dimens.sizeImageStubView, color: App.Colors.black),
           Padding(
             padding: const EdgeInsets.only(left: App.Dimens.bigPadding, right: App.Dimens.bigPadding),
-            child: Text("You don't have any dishes, for dishes addition you have to click +",
+            child: Text(AppTranslations.of(context).text('label_stub_view_dish_list_screen'),
                 textAlign: TextAlign.center, style: App.TextStyles.mediumWhiteText.copyWith(color: App.Colors.black)),
           ),
         ]);
