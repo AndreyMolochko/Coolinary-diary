e1d88ce00e2db2eafe7bd82d7b46d0fe43d960f1
Andrey Molochko <andrey.molochko2018@gmail.com>
2021-04-28 12:50:37 +0300

    Fix bug with navigation

--- lib/ui/request_dish/view_model/request_dish_view_model.dart
+++ lib/ui/request_dish/view_model/request_dish_view_model.dart
@@ -53,8 +53,8 @@ class RequestDishViewModel implements RequestDishViewModelType {
       Navigator
           .pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => AddDishPhotoPage(this)), ModalRoute.withName('/'));
     } else if (requestDishScreenType == RequestDishScreenType.updateDish) {
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => AddDishPhotoPage(this)));
+      Navigator
+          .pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => AddDishPhotoPage(this)), ModalRoute.withName('/'));
     }
   }
 
@@ -65,8 +65,6 @@ class RequestDishViewModel implements RequestDishViewModelType {
       _repository.addDish(dish);
     } else if (requestDishScreenType == RequestDishScreenType.updateDish) {
       _repository.updateDish(dish, previousFilename);
-      Navigator.pushNamedAndRemoveUntil(
-          context, '/dish_info', ModalRoute.withName('/'), arguments: this);
     }
   }
 }
