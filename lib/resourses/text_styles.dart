b2ee9a0aae7f972397a72591c84617a2ce3aba89
Andrey Molochko <andrey.molochko2018@gmail.com>
2021-04-28 13:00:25 +0300

    Change fonts

--- lib/resourses/text_styles.dart
+++ lib/resourses/text_styles.dart
@@ -3,11 +3,11 @@ import 'module.dart' as App;
 
 class TextStyles {
   static const TextStyle mediumWhiteText =
-      TextStyle(color: App.Colors.white, fontSize: App.Dimens.mediumFontSize, fontFamily: 'Latine');
+      TextStyle(color: App.Colors.white, fontSize: App.Dimens.mediumFontSize, fontFamily: 'PTSerif');
 
   static const TextStyle normalBlackText =
-      TextStyle(color: App.Colors.black, fontSize: App.Dimens.normalFontSize, fontFamily: 'Latine');
+      TextStyle(color: App.Colors.black, fontSize: App.Dimens.normalFontSize, fontFamily: 'PTSerif');
 
   static const TextStyle smallBlackText =
-      TextStyle(color: App.Colors.black, fontSize: App.Dimens.smallFontSize, fontFamily: 'Latine');
+      TextStyle(color: App.Colors.black, fontSize: App.Dimens.smallFontSize, fontFamily: 'PTSerif');
 }
